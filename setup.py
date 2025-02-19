11cd975fba7f17116f4b357763cc820f26ae95c6
Saina Shaji <sainashaji2000@gmail.com>
2025-02-19 21:08:41 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'CareMedicalChatBot',
+    version='0.0.0',
+    author='Saina Shaji',
+    author_email='sainashaji2000@gmail.com',
+    packages=find_packages(),
+    install_reuires=[]
+)
